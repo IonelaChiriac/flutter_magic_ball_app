87e0068c3e608814486613443e776fbbaf82c6eb
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-03 22:13:16 +0200

    3 dec first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: class BallPage extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+            return Container();
+          }
+        }
+        ,
+      ),
+    );
