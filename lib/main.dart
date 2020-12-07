3f6a692a4b0d480c3a716e0dd9f57577560fb8ad
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-07 22:11:58 +0200

    7 dec commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: class BallPage extends StatelessWidget {
-          @override
-          Widget build(BuildContext context) {
-            return Container();
-          }
-        }
-        ,
+        home: BallPage(),
       ),
     );
-// comment with Ellie
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.teal.shade900,
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
